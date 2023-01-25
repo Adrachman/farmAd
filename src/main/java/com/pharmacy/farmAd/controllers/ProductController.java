bc5b108ce7146388e139af9a28cb84a8a6bf30b7
Adrachman <adra_admiral@mail.ru>
2023-01-25 13:11:07 +0300

    addPageProduct

--- src/main/java/com/pharmacy/farmAd/controllers/ProductController.java
+++ src/main/java/com/pharmacy/farmAd/controllers/ProductController.java
@@ -1,11 +1,21 @@
 package com.pharmacy.farmAd.controllers;
 
+import com.pharmacy.farmAd.products.Product;
+import com.pharmacy.farmAd.services.ProductService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 public class ProductController {
+    private final ProductService productService;
+
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
 
     @GetMapping("/home")
     public String homePage(Model page){
@@ -13,4 +23,11 @@ public class ProductController {
         page.addAttribute("color", "red");
         return "/home";
     }
+
+   @RequestMapping("/products")
+    public String viewProducts(Model model){
+       List<Product> products = productService.findAll();
+       model.addAttribute("products", products);
+       return "products";
+   }
 }
