6bf8f252715ca9795741e398d2b03b173c195ea1
Adrachman <adra_admiral@mail.ru>
2023-01-25 16:41:01 +0300

    addPageProducts

--- src/main/java/com/pharmacy/farmAd/controllers/ProductController.java
+++ src/main/java/com/pharmacy/farmAd/controllers/ProductController.java
@@ -5,6 +5,7 @@ import com.pharmacy.farmAd.services.ProductService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -24,10 +25,18 @@ public class ProductController {
         return "/home";
     }
 
-   @RequestMapping("/products")
+   @GetMapping("/products")
     public String viewProducts(Model model){
        List<Product> products = productService.findAll();
        model.addAttribute("products", products);
-       return "products";
+       return "products.html";
    }
+
+    @PostMapping("/products")
+    public String addProduct(Product p, Model model){
+        productService.addProduct(p);
+        List<Product> products = productService.findAll();
+        model.addAttribute("products", products);
+        return "products.html";
+    }
 }
--- src/main/java/com/pharmacy/farmAd/services/ProductService.java
+++ src/main/java/com/pharmacy/farmAd/services/ProductService.java
@@ -1,10 +1,12 @@
 package com.pharmacy.farmAd.services;
 
 import com.pharmacy.farmAd.products.Product;
+import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@Service
 public class ProductService {
     private List<Product> products = new ArrayList<>();
 
